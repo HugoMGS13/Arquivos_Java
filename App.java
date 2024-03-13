b4360ae2c05fedcf59bb0e48508ee610168f72bd
HugoMGS13 <ufpahugo@gmail.com>
2024-03-13 17:29:08 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        String add;
+
+        for (int i = 1; i <= 10; i++){
+            if (i % 2 == 0)
+                add = "*";
+            else
+                add = "";
+            System.out.println(i+add);
+        }
+        
     }
 }
